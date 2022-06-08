6c272a0faf381c70ac48cbfec345861c13e281d7
roquiles <roberta_btg@hotmail.com>
2022-06-08 20:48:17 -0300

    Add new sections to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import TravelTypes from "../components/TravelTypes";
+import CallToAction from "../components/CallToAction";
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
       <Banner />
+      <TravelTypes />
+      <CallToAction />
     </>
   );
 };
