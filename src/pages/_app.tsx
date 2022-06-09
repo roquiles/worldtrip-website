d47e7398240d85d52a06caa9f6e707050f9ece2d
roquiles <roberta_btg@hotmail.com>
2022-06-09 18:40:55 -0300

    Add ContinentsProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import ContinentsProvider from "../contexts/Continents/provider";
 import { theme } from "../styles/theme";
+
+import "./../styles/slider.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ContinentsProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ContinentsProvider>
   );
 }
 
