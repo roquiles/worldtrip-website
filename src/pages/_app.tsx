47da90f4a38bb6060c5e4aec80d2dd845ff073ad
roquiles <roberta_btg@hotmail.com>
2022-06-10 23:25:05 -0300

    Add header as a global component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import ContinentsProvider from "../contexts/Continents/provider";
 import { theme } from "../styles/theme";
 
 import "./../styles/slider.css";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ContinentsProvider>
       <ChakraProvider theme={theme}>
+        <Header />
         <Component {...pageProps} />
       </ChakraProvider>
     </ContinentsProvider>
