ba6579d64cadff163e8d5c15d3f7447571080869
roquiles <roberta_btg@hotmail.com>
2022-06-13 18:48:04 -0300

    Create City's interface and update continent's type

--- src/contexts/Continents/provider.tsx
+++ src/contexts/Continents/provider.tsx
@@ -5,12 +5,24 @@ interface ContinentsProviderProps {
   children: ReactNode;
 }
 
+export interface CityType {
+  name: string;
+  country: string;
+  imageURL: string;
+  flag: string;
+}
+
 export interface ContinentType {
   id: number;
   name: string;
-  description: string;
+  subtitle: string;
   imageURL: string;
   bannerURL: string;
+  description: string;
+  countriesCount: number;
+  languagesCount: number;
+  listedOnTop100: number;
+  citiesListedOnTop100: CityType[];
 }
 
 export default function ContinentsProvider({
