1352c4c4b42326581046dc4cd2512ff2c3ae0361
roquiles <roberta_btg@hotmail.com>
2022-06-13 18:49:52 -0300

    Create continent's page

---
+++ src/pages/[continentId].tsx
@@ -0,0 +1,69 @@
+import { Flex, Heading, VStack, SimpleGrid } from "@chakra-ui/react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { ContinentType } from "../contexts/Continents/provider";
+import BannerContinent from "../components/BannerContinent";
+import ContinentInfos from "../components/ContinentInfos";
+import CityCard from "../components/CityCard";
+
+interface ContinentsPageProps {
+  selectedContinentData: ContinentType;
+}
+
+export default function ContinentPage({
+  selectedContinentData,
+}: ContinentsPageProps) {
+  return (
+    <VStack display="flex" justify="center" width="full">
+      <BannerContinent continent={selectedContinentData} />
+      <ContinentInfos continent={selectedContinentData} />
+
+      <Flex direction="column" width="full" maxW="1160px">
+        <Heading width="full" fontWeight="500">
+          Cidades +100
+        </Heading>
+
+        <SimpleGrid columns={4} marginY="10">
+          {selectedContinentData.citiesListedOnTop100.map((city) => (
+            <CityCard city={city} key={city.name} />
+          ))}
+        </SimpleGrid>
+      </Flex>
+    </VStack>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const continents = await fetch(
+    "https://my-json-server.typicode.com/roquiles/worldtrip-website/continentes"
+  ).then((response) => response.json());
+
+  const paths = continents.map((continentItem: ContinentType) => {
+    return { params: { continentId: String(continentItem.id) } };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const params = context.params;
+
+  const selectedContinentData = await fetch(
+    `https://my-json-server.typicode.com/roquiles/worldtrip-website/continentes/`
+  )
+    .then((response) => response.json())
+    .then((continents) =>
+      continents.find(
+        (continentItem: ContinentType) =>
+          String(continentItem.id) === params?.continentId
+      )
+    );
+
+  return {
+    props: {
+      selectedContinentData,
+    },
+  };
+};
